5ddfe0993630e67e9314822878625aa68a44c9c9
gerrul3r <shadow_tsw@web.de>
2020-06-29 14:33:33 +0200

    preparing launch

--- websocket-server.js
+++ websocket-server.js
@@ -1,27 +1,23 @@
 
 const WebSocketServer = require('ws')
-const { parse } = require('path')
 
 // import WebSocket from "ws"
 
 // Create a WebSocket server by defining its port number.
-const server = new WebSocketServer.Server({ port: 3333 })
+const wss = new WebSocketServer.Server({ port: 3333 })
 
 const botManager = new Map()
 const userManager = new Map()
 
-server.on('connection', connection => {
+wss.on('connection', (connection) => {
+    console.log('Client connected');
+    connection.on('close', () => console.log('Client disconnected'));
 
-    // console.log(connection)
-    // Listen for incoming messages and run the defined
-    // callback function when a messages comes in. 
     connection.on('message', (message) => {
-
-        // console.log(message);
-        console.log(JSON.parse(message));
 
         //? Message parser:
         const parsedMessage = JSON.parse(message)
+        console.log(parsedMessage);
 
         //?Identify Message type
         //! MessageTypes
@@ -41,24 +37,25 @@ server.on('connection', connection => {
                     conn.send(JSON.stringify(`Anzahl registrierter Bots: ${botManager.size}`))
                 }
                 break;
-             //!Push new Ads to Users
+            //!Push new Ads to Users
             case "PUSH Ads":
-                server.clients.forEach((client) => {
+                wss.clients.forEach((client) => {
                     if (client.readyState === WebSocketServer.OPEN) {
                         client.send(JSON.stringify({ handleServerAction: "fetchLocations" }))
                     }
                 })
             //!Register New/LoggedIn User
             case "NEW User":
-                    if (parsedMessage.register) {
-                        if (!userManager.get(parsedMessage.username)) {
-                            userManager.set(parsedMessage.username, connection)
-                            connection.send(JSON.stringify({topic:"Willkommen",message:`Hallo ${parsedMessage.username}, schön, dass Sie hier sind`}))
-                        }
+                if (parsedMessage.register) {
+                    if (!userManager.get(parsedMessage.username)) {
+                        userManager.set(parsedMessage.username, connection)
+                        connection.send(JSON.stringify({ topic: "Willkommen", message: `Hallo ${parsedMessage.username}, schön, dass Sie hier sind` }))
+                    }
                 }
             default:
-                console.log("Unbekannte Handlung")
+                console.log(parsedMessage)
+                console.log(new Date())
                 break;
         }
-    })
-})
+    });
+});
